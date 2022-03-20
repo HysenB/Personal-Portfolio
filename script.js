e4613d5610b0cb5d6069298cc30bcd8112bf0ed1
HysenB <hysi2000@live.com>
2022-03-20 05:01:56 +0100

    Adding Project Section Updating Contact Section Adding Background particles(animation)

--- script.js
+++ script.js
@@ -4,33 +4,66 @@ const overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
   })
-})
+});
+
 
 overlay.addEventListener('click', () => {
   const modals = document.querySelectorAll('.modal.active')
   modals.forEach(modal => {
-    closeModal(modal)
+    closeModal(modal);
   })
-})
+});
 
 closeModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
+    const modal = button.closest('.modal');
+    closeModal(modal);
   })
-})
+});
 
 function openModal(modal) {
   if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+  modal.classList.add('active');
+  overlay.classList.add('active');
 }
 
 function closeModal(modal) {
   if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 70}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }
